3da1899466989b4f44e5a6be014ae379fdebaf11
artis@igstudija.lv <artis@igstudija.lv>
2024-06-04 12:05:04 +0300

    first commit

---
+++ .storybook/NumoTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Numo Storybook',
+    brandUrl: '/',
+    brandImage: 'numo.svg',
+    brandTarget: '_self',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import numoTheme from './NumoTheme';
+
+addons.setConfig({
+    theme: numoTheme,
+});
